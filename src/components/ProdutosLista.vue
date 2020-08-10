628ea1da4575b44d7cd49ffabc47d5d445f5b09a
Hugo Pedro <hugopedro098@gmail.com>
2020-08-10 16:18:31 -0300

    Axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import { api } from "@/services.js";
 export default {
   data() {
     return {
@@ -18,11 +19,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto")
-        .then((response) => response.json())
-        .then((response) => {
-          this.produtos = response;
-        });
+      api.get("/produto").then((response) => {
+        this.produtos = response.data;
+      });
     },
   },
   created() {
