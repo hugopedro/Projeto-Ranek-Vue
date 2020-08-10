a25ca2bf980d860d2b9f0e6ed1e1f91aedfc3cc0
Hugo Pedro <hugopedro098@gmail.com>
2020-08-10 14:05:14 -0300

    Construindo Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,27 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍Compre ou Venda👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
 export default {
   name: "home",
+  components: { ProdutosBuscar, ProdutosLista },
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
