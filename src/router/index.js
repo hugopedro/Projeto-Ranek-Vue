f8ce95b2ab085d7a7990084d83f444790567af5d
Hugo Pedro <hugopedro098@gmail.com>
2020-08-11 11:45:33 -0300

    Animando a transição da página

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  },
 });
+
+//o scrollBehavior é pra quando mudar de página a próxima pagina não iniciar de lá de baixo, entao ele volta pro topo de forma suave
 
 export default router;
