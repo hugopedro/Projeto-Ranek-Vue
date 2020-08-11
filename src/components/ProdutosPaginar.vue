96931e8a8a5493fd7d5377e96b43808817b759b3
Hugo Pedro <hugopedro098@gmail.com>
2020-08-11 11:11:44 -0300

    Construindo o Range da Paginação

--- src/components/ProdutosPaginar.vue
+++ src/components/ProdutosPaginar.vue
@@ -1,6 +1,7 @@
 <template>
   <ul v-if="paginasTotal > 1">
-    <li v-for="pagina in paginasTotal" :key="pagina">
+    <!-- paginas porque agora é a página dentro da array em sí-->
+    <li v-for="pagina in paginas" :key="pagina">
       <router-link :to="{query: query(pagina)}">{{pagina}}</router-link>
     </li>
   </ul>
@@ -28,6 +29,25 @@ export default {
     },
   },
   computed: {
+    paginas() {
+      //retorna um array com o total que eu quero que mostre no momento
+      //primeiro precisa saber em qual página o usuário está no momento
+      const current = Number(this.$route.query._page);
+      //agora o total de paginas
+      const range = 9;
+      const offset = Math.ceil(range / 2);
+      const total = this.paginasTotal;
+      const pagesArray = [];
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i);
+      }
+      //o splice recebe 2 argumentos, daonde se começa a remover item do array e até onde eu removo
+      pagesArray.splice(0, current - offset); // menos offset pra mostrar as 5 ultimas
+      pagesArray.splice(range, total); // começa do range(ultimo item) até o total, daí ele remove até o ultimo, pra nao fica aparecendo numeros das proximas pag
+
+      return pagesArray;
+    },
     paginasTotal() {
       const total = this.produtosTotal / this.produtosPorPagina;
       return total !== Infinity ? Math.ceil(total) : 0; //  arredonda pra cima
